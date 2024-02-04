4352ef2f047c44f6936457eaa7351749d4d2887f
JOSE FONSECA MENDEZ <jose.fonseca.mendez@est.una.ac.cr>
2024-02-04 01:21:47 -0600

    baseURL switched

--- src/Api/apiAxios.js
+++ src/Api/apiAxios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //aseURL: 'https://coopepilangostarl.azurewebsites.net/api/',
-  baseURL: 'https://localhost:7275/api/',
+  baseURL: 'https://coopepilangostarl.azurewebsites.net/api/',
+  //baseURL: 'https://localhost:7275/api/',
   headers: {
     Authorization:`Bearer ${localStorage.getItem('bearer')}`
   }
