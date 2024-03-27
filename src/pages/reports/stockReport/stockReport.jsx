d563e9a559dd80588decd1231d1ee5af8f04f5fb
Kuc <kucsen2003@gmail.com>
2024-03-27 08:44:53 -0600

    -stockReport Nueva tabla

--- src/pages/reports/stockReport/stockReport.jsx
+++ src/pages/reports/stockReport/stockReport.jsx
@@ -1,174 +1,126 @@
-import React, { useState } from "react";
+
+import { useState, useEffect, useMemo } from "react";
+import { Button} from '@mui/material';
+import { MaterialReactTable} from 'material-react-table';
+import useCustomMaterialTable from '../../../utils/materialTableConfig.js'; 
+import autoTable from 'jspdf-autotable';
+import { jsPDF } from 'jspdf'; 
+import { format } from "date-fns";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import { getstocks } from "../../../services/reportServices/stockreportService";
-import { useQuery } from "react-query";
-import { Table, Form, Container, Col, Row, Button } from "react-bootstrap";
-import ReactPaginate from "react-paginate";
+import { Container } from "react-bootstrap";
 import "../../../css/StylesBtn.css";
 import "../../../css/Pagination.css";
-import { format } from "date-fns";
 import { validateAllowedPageAccess } from "../../../utils/validatePageAccess";
-import { useEffect } from "react";
 
-const stockReport = () => {
+const MaterialTable = () => {
+
+  const [data, setData] = useState([]);
+
+
   useEffect(() => {
-    validateAllowedPageAccess();
+    const fetchData = async () => {
+      try {
+        const response = await getstocks();
+        setData(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
   }, []);
 
-  const {
-    data: stocksData,
-    isLoading: stocksLoading,
-    isError: stocksError,
-  } = useQuery("stockreport", getstocks);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const { isError: isLoadingError, isFetching: isFetching, isLoading: isLoading } = getstocks();
 
-  if (stocksLoading) {
-    return (
-      <div>
-        <div className="Loading">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+  const columns = useMemo(() => [
+    {
+      accessorKey: 'productName',
+      header: 'Producto',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'cambioFecha',
+      header: 'Fecha del cambio',
+      enableClickToCopy: true,
+      Cell: ({ row }) => { 
+        return (<span>{row.original.cambioFecha === "0001-01-01T00:00:00" ? "" : format(new Date(row.original.cambioFecha), "yyyy-MM-dd")}</span>);
+      }
+    },
+    {
+      accessorKey: 'oldStock',
+      header: 'Inicial',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'newStock',
+      header: 'Modificado',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'motive',
+      header: 'Motivo',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'email',
+      header: 'Usuario',
+      enableClickToCopy: true,
+    },
+  ], []);
 
-  if (stocksError) {
-    return <div>Error al cargar los datos.</div>;
-  }
+  const handleExportRows = (rows) => {
+    const doc = new jsPDF();
+    const tableData = rows.map((row) => 
+    Object.values(row.original));
+    const tableHeaders = columns.map((c) => c.header);
 
-  const filteredBySearch = stocksData.filter(
-    (stock) =>
-      stock.productName
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f\s]/g, "")
-        .toLowerCase()
-        .includes(searchTerm.replace(/\s/g, "").toLowerCase()) ||
-      stock.motive
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f\s]/g, "")
-        .toLowerCase()
-        .includes(searchTerm.replace(/\s/g, "").toLowerCase()) ||
-      stock.email
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f\s]/g, "")
-        .toLowerCase()
-        .includes(searchTerm.replace(/\s/g, "").toLowerCase())
-  );
+    autoTable(doc, {
+      head: [tableHeaders],
+      body: tableData,
+    });
 
-  const filteredByDate = filteredBySearch.filter((stock) => {
-    if (startDate && endDate) {
-      const stockDate = new Date(stock.cambioFecha);
-      const start = new Date(startDate);
-      const end = new Date(endDate);
-      return stockDate >= start && stockDate <= end;
-    }
-    return true;
-  });
-
-  const recordsPerPage = 10;
-  const offset = currentPage * recordsPerPage;
-  const paginatedStocks = filteredByDate.slice(offset, offset + recordsPerPage);
-
-  const pageCount = Math.ceil(filteredByDate.length / recordsPerPage);
-
-  const handlePageClick = (data) => {
-    setCurrentPage(data.selected);
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "yyyy-MM-dd");
+    doc.save(`Reporte Cambio de Existencias ${formattedDate}.pdf`);
   };
 
-  return (
-    <Container>
-      <div className="table-container">
-        <h2 className="table-title">Historial de Inventario</h2>
-        <hr className="divider" />
+  const table = useCustomMaterialTable({
+    columns,
+    data: data,
+    isLoading,
+    isLoadingError,
+    isFetching,
 
-        <br></br>
+    renderTopToolbarCustomActions: ({ table }) =>(
+    <>
 
-        <Form>
-          <Row className="mb-3 filters-container">
-            <Col xs={6} md={3}>
-              <Form.Control
-                type="text"
-                placeholder="Buscar coincidencias"
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="filter-input"
-              />
-            </Col>
-            <Col xs={6} md={3}>
-              <Form.Label>Fecha Inicial</Form.Label>
-              <Form.Control
-                type="datetime-local"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-            </Col>
-            <Col xs={6} md={3}>
-              <Form.Label>Fecha Final</Form.Label>
-              <Form.Control
-                type="datetime-local"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form>
+    <Button
+      disabled={table.getPrePaginationRowModel().rows.length === 0}
+      onClick={() => handleExportRows(table.getPrePaginationRowModel().rows)}
+      startIcon={<FileDownloadIcon />}
+    >
+      Exportar
+    </Button></>),
+  });
 
-        <Col xs={12} md={2} lg={12}>
-          {stocksData ? (
-            <Row>
-              <Table className="Table" responsive>
-                <thead>
-                  <tr>
-                    <th>Producto</th>
-                    <th>Fecha</th>
-                    <th>Inicial</th>
-                    <th>Modificado</th>
-                    <th>Motivo</th>
-                    <th>Usuario</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {paginatedStocks.map((stock) => (
-                    <tr key={stock.Id}>
-                      <td>{stock.productName}</td>
-                      <td>
-                        {format(new Date(stock.cambioFecha), "dd-MM-yyyy")}
-                      </td>
-                      <td>{stock.oldStock}</td>
-                      <td>{stock.newStock}</td>
-                      <td>{stock.motive}</td>
-                      <td>{stock.email}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-              <div className="Pagination-Container">
-                <ReactPaginate
-                  previousLabel={"<"}
-                  nextLabel={">"}
-                  breakLabel={"..."}
-                  pageCount={pageCount}
-                  marginPagesDisplayed={2}
-                  pageRangeDisplayed={5}
-                  onPageChange={handlePageClick}
-                  containerClassName={"pagination"}
-                  subContainerClassName={"pages pagination"}
-                  activeClassName={"active"}
-                />
-              </div>
-            </Row>
-          ) : (
-            "Cargando"
-          )}
-        </Col>
-      </div>
-    </Container>
-  );
+  return <MaterialReactTable table={table}
+  />;
 };
+
+const queryClient = new QueryClient();
+
+const stockReport = () => (
+  <Container>
+    <div className="table-container">
+      <h2 className="table-title">Historial de Inventario</h2>
+      <hr className="divider" />
+      <QueryClientProvider client={queryClient}>
+        <MaterialTable />
+      </QueryClientProvider>
+    </div>
+  </Container>
+
+);
 
 export default stockReport;
