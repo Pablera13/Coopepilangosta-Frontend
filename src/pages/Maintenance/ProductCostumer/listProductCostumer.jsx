527a6ddcee801bb11b1b6a2fa9d55b652756c6e1
Kuc <kucsen2003@gmail.com>
2024-03-28 19:40:20 -0600

    listProductCostumer tabla nueva prueba

--- src/pages/Maintenance/ProductCostumer/listProductCostumer.jsx
+++ src/pages/Maintenance/ProductCostumer/listProductCostumer.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect, useMemo } from "react";
+import { Box, Button, Tooltip} from '@mui/material';
+import { MaterialReactTable} from 'material-react-table';
+import useCustomMaterialTable from '../../../utils/materialTableConfig.js'; 
+import autoTable from 'jspdf-autotable';
+import { jsPDF } from 'jspdf'; 
+import { format } from "date-fns";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Container } from "react-bootstrap";
 import { getProductCostumer } from "../../../services/productCostumerService.js";
-import { NavLink } from "react-router-dom";
 import { deleteProductCostumer } from "../../../services/productCostumerService.js";
-import { Table, Container, Col, Row, Button, Form } from "react-bootstrap";
 import AddProductCostumer from "./actions/addProductCostumer.jsx";
-import ReactPaginate from "react-paginate";
-import syles from "../ProductCostumer/listProductCostumer.css";
-import { useNavigate, useParams } from "react-router-dom";
 import UpdateProductCostumer from "./actions/updateProductCostumer";
 import VolumeDiscountModal from "./actions/volumeDiscountModal";
 import ExportProductCostumer from "./actions/exportProductCostumer";
@@ -17,202 +20,164 @@ import "../../../css/Pagination.css";
 import "../../../css/StylesBtn.css";
 import { validateAllowedPageAccess } from "../../../utils/validatePageAccess.js";
 
-const listProductCostumer = () => {
-  const Params = useParams();
+const MaterialTable = () => {
 
-  useEffect(() => {
-    validateAllowedPageAccess()
-  }, [])
-  
-  const [ProductCostumers, setProductCostumers] = useState([]);
-  const [Cotizaciones, setCotizaciones] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const navigate = useNavigate();
-  const recordsPerPage = 6;
-  const [currentPage, setCurrentPage] = useState(0);
-
-  useEffect(() => {
-    async function obtainProductCostumer() {
-      await getProductCostumer(Params.costumerid, setProductCostumers);
-    }
-    obtainProductCostumer();
-  }, [Params.costumerid]);
-
-  useEffect(() => {
-    if (ProductCostumers && ProductCostumers.length > 0) {
-      ObtainCotizaciones();
-    }
-  }, [ProductCostumers]);
-
-  const ObtainCotizaciones = async () => {
-    if (ProductCostumers) {
-      let cotizaciones = [];
-      for (const productcostumer of ProductCostumers) {
-        const product = await getProductById2(productcostumer.productId);
-
-        const MargenGanancia =
-          productcostumer.purchasePrice * (productcostumer.margin / 100);
-        const PrecioConMargen = productcostumer.purchasePrice + MargenGanancia;
-        const IVA = PrecioConMargen * (product.iva / 100);
-        const PrecioFinal = PrecioConMargen + IVA;
-
-        let cotizacion = {
-          id: productcostumer.id,
-          productId: product.id,
-          productName: product.name,
-          productUnit: productcostumer.unit,
-          productIva: product.iva,
-          finalPrice: PrecioFinal.toFixed(0),
-          costumerId: Params.costumerid,
-          purchasePrice: productcostumer.purchasePrice,
-          description: productcostumer.description,
-          margin: productcostumer.margin,
-        };
-        cotizaciones.push(cotizacion);
-      }
-      setCotizaciones(cotizaciones);
-    }
-  };
-
-  const filteredCotizaciones = Cotizaciones.filter(
-    (cotizacion) =>
-      cotizacion.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      cotizacion.productUnit.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const offset = currentPage * recordsPerPage;
-  const paginatedCotizaciones = filteredCotizaciones.slice(
-    offset,
-    offset + recordsPerPage
-  );
-
-  const pageCount = Math.ceil(Cotizaciones.length / recordsPerPage);
-
-  const handlePageClick = (data) => {
-    setCurrentPage(data.selected);
-    console.log("paginatedCotizaciones = " + paginatedCotizaciones.length);
-  };
+  const [data, setData] = useState([]);
 
   const showAlert = (id) => {
     swal({
       title: "Eliminar",
-      text: "¿Está seguro de que desea eliminar esta cotización?",
+      text: "¿Está seguro de que desea eliminar este producto?",
       icon: "warning",
       buttons: ["Cancelar", "Aceptar"],
     }).then((answer) => {
       if (answer) {
         deleteProductCostumer(id);
         swal({
-          title: "Eliminado",
-          text: "La cotización ha sido eliminada",
-          icon: "success",
-        }).then(function () {
-          window.location.reload();
-        });
+          title: 'Eliminado',
+          text: 'El producto ha sido eliminado',
+          icon: 'success',
+        }).then(function () { window.location.reload() });
+
       }
     });
   };
 
-  return (
-    <Container>
-      <div className="table-container">
-        <h2 className="table-title">
-          Cotizaciones para {Params.costumername}{" "}
-        </h2>
-        <hr className="divider" />
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getProductCostumer();
+        setData(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
-        <br></br>
+  const { isError: isLoadingError, isFetching: isFetching, isLoading: isLoading } = getProductCostumer();
 
-        <Form>
-          <Row className="mb-3 filters-container">
-          <Col xs={6} md={6}>
-              <AddProductCostumer />
-            </Col>
-            <Col xs={0} md={0}></Col>
-            <Col xs={12} md={3}>
-              <Form.Control
-                type="text"
-                placeholder="Por nombre o unidad comercial..."
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="filter-input"
-              />
-            </Col>
-          </Row>
-        </Form>
+  const columns = useMemo(() => [
+    {
+      accessorKey: 'productName',
+      header: 'Producto',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'productUnit',
+      header: 'Unidad',
+      enableClickToCopy: true,
+    },
+    {
+      accessorKey: 'purchasePrice',
+      header: 'Precio inicial	',
+      enableClickToCopy: true,
+      Cell: ({ row }) => { return (<span>{`₡${row.original.purchasePrice}`}</span>); }
+    },
+    {
+      accessorKey: 'margin',
+      header: 'Margen (%)',
+      enableClickToCopy: true,
+      Cell: ({ row }) => { return (<span>{`${row.original.margin}%`}</span>); }
+    },
+    {
+      accessorKey: 'productIva',
+      header: 'IVA',
+      enableClickToCopy: true,
+      Cell: ({ row }) => { return (<span>{`${row.original.productIva}%`}</span>); }
+    },
+    {
+      accessorKey: 'finalPrice',
+      header: 'Precio Final',
+      enableClickToCopy: true,
+      Cell: ({ row }) => { return (<span>{`₡${row.original.finalPrice}`}</span>); }
+    },
+    {
+      accessorKey: 'description',
+      header: 'Descripción',
+      enableClickToCopy: true,
+    },
+  ], []);
 
-        <Col xs={12} md={2} lg={12}>
-          {Cotizaciones && Cotizaciones.length > 0 ? (
-            <Row>
-              <Table
-                className="Table"
-                responsive
-              >
-                <thead>
-                  <tr>
-                    <th>Producto</th>
-                    <th>Unidad</th>
-                    <th>Precio inicial</th>
-                    <th>Margen</th>
-                    <th>IVA</th>
-                    <th>Precio Final</th>
-                    <th>Descripción</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {paginatedCotizaciones.map((cotizacion) => (
-          
-                    <tr>
-                      <td>{cotizacion.productName}</td>
-                      <td>{cotizacion.productUnit}</td>
-                      <td>₡{cotizacion.purchasePrice}</td>
-                      <td>{cotizacion.margin}%</td>
-                      <td>{cotizacion.productIva}%</td>
-                      <td>₡{cotizacion.finalPrice}</td>
-                      <td>{cotizacion.description}</td>
-                      <td>
-                        <div className="BtnContainer">
-                          <UpdateProductCostumer props={cotizacion} />
+  const handleExportRows = (rows) => {
+    const doc = new jsPDF();
+    const tableData = rows.map((row) => 
+    Object.values(row.original));
+    const tableHeaders = columns.map((c) => c.header);
 
-                          <Button
-                            className="BtnRed"
-                            onClick={() => showAlert(cotizacion.id)}
-                          >
-                            <MdDelete />
-                          </Button>
+    autoTable(doc, {
+      head: [tableHeaders],
+      body: tableData,
+    });
 
-                          <VolumeDiscountModal props={cotizacion.id} />
-                          <ExportProductCostumer props={cotizacion} />
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "yyyy-MM-dd");
+    doc.save(`Reporte Cotizaciones ${formattedDate}.pdf`);
+  };
 
-              <div className="Pagination-Container">
-                <ReactPaginate
-                  previousLabel={"<"}
-                  nextLabel={">"}
-                  breakLabel={"..."}
-                  pageCount={pageCount}
-                  marginPagesDisplayed={2}
-                  pageRangeDisplayed={5}
-                  onPageChange={handlePageClick}
-                  containerClassName={"pagination"}
-                  subContainerClassName={"pages pagination"}
-                  activeClassName={"active"}
-                />
-              </div>
-            </Row>
-          ) : (
-            "Cargando"
-          )}
-        </Col>
-      </div>
-    </Container>
-  );
+  const table = useCustomMaterialTable({
+    columns,
+    data: data,
+    isLoading,
+    isLoadingError,
+    isFetching,
+    showAlert,
+
+    renderRowActions: ({row}) => (
+      <Box sx={{ display: 'flex', gap: '1rem' }}>
+        <Tooltip title="Actualizar cotización">
+
+          <UpdateProductCostumer props={row.original} />
+
+        </Tooltip>
+        <Tooltip title="Eliminar">
+
+          <Button className="BtnRed" onClick={() => showAlert(row.original.id)}><MdDelete /></Button>
+
+        </Tooltip>
+        <Tooltip title="Descuentos por volumen">
+
+          <VolumeDiscountModal props={row.original.id} />
+
+        </Tooltip>
+        <Tooltip title="Exportar cotización">
+
+          <ExportProductCostumer props={row.original} />
+
+        </Tooltip>
+      </Box>
+    ),
+
+    renderTopToolbarCustomActions: ({ table }) =>(
+    <>
+    <AddProductCostumer/>
+
+    <Button
+      disabled={table.getPrePaginationRowModel().rows.length === 0}
+      onClick={() => handleExportRows(table.getPrePaginationRowModel().rows)}
+      startIcon={<FileDownloadIcon />}
+    >
+      Exportar
+    </Button></>),
+  });
+
+  return <MaterialReactTable table={table}
+  />;
 };
+
+const queryClient = new QueryClient();
+
+const listProductCostumer = () => (
+  <Container>
+    <div className="table-container">
+      <h2 className="table-title">Cotizaciones</h2>
+      <hr className="divider" />
+      <QueryClientProvider client={queryClient}>
+        <MaterialTable />
+      </QueryClientProvider>
+    </div>
+  </Container>
+
+);
 
 export default listProductCostumer;
